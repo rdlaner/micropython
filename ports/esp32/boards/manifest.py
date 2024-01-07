f7084e8a4055c7d74353b6811c2867df49bb9560
Dustin L <r.dustin.lane@gmail.com>
2024-01-07 11:58:48 -0800

    Remove unneeded required modules from port manifest

--- ports/esp32/boards/manifest.py
+++ ports/esp32/boards/manifest.py
@@ -6,10 +6,5 @@ require("bundle-networking")
 
 # Require some micropython-lib modules.
 require("aioespnow")
-require("dht")
-require("ds18x20")
 require("neopixel")
-require("onewire")
-require("umqtt.robust")
-require("umqtt.simple")
 require("upysh")
